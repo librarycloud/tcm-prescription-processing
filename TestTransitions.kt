e826859839e424f2de1b71ecfdc0cccb56df8c1c
IngressPrime <IngressPrime@users.noreply.github.com>
2026-09-11 18:38:30 +0800

    chore: remove test files

--- TestTransitions.kt
+++
@@ -1,7 +0,0 @@
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
-
-fun test() {
-    val e = EnterTransition.None
-    val x = ExitTransition.None
-}
