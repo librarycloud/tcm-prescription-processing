2f5e98852d822f201a6297d71fd186a5b8755ad0
IngressPrime <IngressPrime@users.noreply.github.com>
2026-09-25 20:47:57 +0800

    feat(ios): cap image upload resolution to 12MP (4000px) and restore 0.85 jpeg quality

--- ios-app/TCMAdmin/Views/Components/AppComponents.swift
+++ ios-app/TCMAdmin/Views/Components/AppComponents.swift
@@ -17,8 +17,8 @@ func downsampledImage(from data: Data, maxPixelSize: CGFloat) -> UIImage? {
 nonisolated func jpegDataForUpload(
     from cgImage: CGImage,
     orientation: CGImagePropertyOrientation,
-    maxPixelSize: CGFloat = 10000,
-    quality: CGFloat = 0.95
+    maxPixelSize: CGFloat = 4000,
+    quality: CGFloat = 0.85
 ) -> Data? {
     let sourceWidth = CGFloat(cgImage.width)
     let sourceHeight = CGFloat(cgImage.height)
